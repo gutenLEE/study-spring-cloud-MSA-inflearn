bd842f662fb968de93498484f6c1d2e35f93b348
gutenLEE <bnm1128@gmail.com>
2021-04-26 18:15:56 +0900

    load balancer 추가 직전

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LogginFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LogginFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,7 @@ public class LogginFilter extends AbstractGatewayFilterFactory<LogginFilter.Conf
     public GatewayFilter apply(Config config) {
 
         // custom pre filter
+/*
         return ((exchange, chain) ->{
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
@@ -37,10 +40,26 @@ public class LogginFilter extends AbstractGatewayFilterFactory<LogginFilter.Conf
                 }
             }));
         });
+*/
 
         // 위 람다식은 아래 두 코드를 합친 것.
-/*        GatewayFilter filter = null;
-        return filter;*/
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) ->{
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage : request id -> {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){
+                log.info("Logging Pre Filter Start : request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if(config.isPostLogger()){
+                    log.info("Logging POST Filter end : response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, Ordered.HIGHEST_PRECEDENCE);
+        return filter;
     }
 
     @Data
