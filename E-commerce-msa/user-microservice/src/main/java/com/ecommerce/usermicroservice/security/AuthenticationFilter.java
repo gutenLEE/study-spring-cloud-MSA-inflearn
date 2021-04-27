fe1258fc2283ec2cf1f381164a3ccbdc2f7e44cb
gutenLEE <bnm1128@gmail.com>
2021-04-27 18:43:58 +0900

    인증 authenticationFilter 추가

---
+++ E-commerce-msa/user-microservice/src/main/java/com/ecommerce/usermicroservice/security/AuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.ecommerce.usermicroservice.security;
+
+import com.ecommerce.usermicroservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        try {
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            return getAuthenticationManager().authenticate( // 인증 정보를 AuthenticationManager에게 넘겨 로그인 요청
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
--- E-commerce-msa/user-microservice/src/main/java/com/ecommerce/usermicroservice/security/WebSecurity.java
+++ E-commerce-msa/user-microservice/src/main/java/com/ecommerce/usermicroservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -13,8 +15,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/users/**").permitAll();
+                .authorizeRequests().antMatchers("/**")
+                .hasIpAddress("10.120.120.144")
+                .and()
+                .addFilter(getAuthenticationFilter());
+        //        .authorizeRequests().antMatchers("/users/**").permitAll();
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
